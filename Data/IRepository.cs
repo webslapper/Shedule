a353bd67a4980d67a4be2af3066de550d7dc9b35
Vuyko Sheshych <vuykosheshych@gmail.com>
2025-02-07 17:04:45 +0200

    refactor(DbContext): remove code dublications in SetDataAsync method

--- Data/IRepository.cs
+++ Data/IRepository.cs
@@ -1,10 +1,13 @@
+using System.Linq.Expressions;
+
 namespace Shedule.Data;
 
-public interface IRepository<T> where T: class
+public interface IRepository<T> where T : class
 {
-    Task<T> GetByIdAsync(int id);
-    Task<List<T>> GetAllAsync();
-    Task AddAsync(T entity);
-    Task UpdateAsync(T entity);
-    Task DeleteAsync(int id);
+	Task<List<T>> GetByConditionAsync(Expression<Func<T, bool>> predicate);
+	Task<T> GetByIdAsync(int id);
+	Task<List<T>> GetAllAsync();
+	Task AddAsync(T entity);
+	Task UpdateAsync(T entity);
+	Task DeleteAsync(int id);
 }
